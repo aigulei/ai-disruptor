821b608ae048806ace3d3d6a3d381d9d244f1d27
aigulei <touchjava@126.com>
2019-03-06 15:51:39 +0800

    first learn disruptor

---
+++ src/main/java/com/ai/disruptor/App.java
@@ -0,0 +1,13 @@
+package com.ai.disruptor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
